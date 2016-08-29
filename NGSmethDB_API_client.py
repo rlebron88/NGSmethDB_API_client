f27e3130c371fe8332579a87f92a9ad82ac63fae
Ricardo Lebrón <rlebron88@gmail.com>
2016-08-29 13:38:17 +0200

    Update NGSmethDB_API_client.py

--- NGSmethDB_API_client.py
+++ NGSmethDB_API_client.py
@@ -259,12 +259,12 @@ def get_region(index, total, region, assembly, samples, output, server, bar):
             has_interindividual = False
             if not os.path.exists(intraindividual_file):
                 with open(os.path.join(diffmeth_cg, 'intraindividual' + '.tsv'), 'wt') as handle:
-                    header = ['chrom', 'pos', 'methContext', 'sample1', 'sample2', 'method', 'pValue']
+                    header = ['chrom', 'pos', 'methContext', 'sample1', 'sample2', 'method', 'pValue', 'consensus']
                     header = '\t'.join(header) + '\n'
                     handle.write(header)
             if not os.path.exists(interindividual_file):
                 with open(os.path.join(diffmeth_cg, 'interindividual' + '.tsv'), 'wt') as handle:
-                    header = ['chrom', 'pos', 'methContext', 'sample1', 'sample2', 'method', 'consensus', 'pValue']
+                    header = ['chrom', 'pos', 'methContext', 'sample1', 'sample2', 'method', 'pValue', 'consensus']
                     header = '\t'.join(header) + '\n'
                     handle.write(header)
             for pair in list(itertools.combinations(samples, 2)):
@@ -284,7 +284,7 @@ def get_region(index, total, region, assembly, samples, output, server, bar):
                             pvalues = d['diffmeth_cg'][individual_pair][sample_pair]
                             for method in pvalues:
                                 pvalue = pvalues[method]
-                                line = [d['chrom'], d['pos'], 'CG', tmp1, tmp2, method, 'True' if len(pvalues) == 3 else 'False', pvalue]
+                                line = [d['chrom'], d['pos'], 'CG', tmp1, tmp2, method, pvalue, 'True' if len(pvalues) == 3 else 'False']
                                 line = [str(value) if value else '.' for value in line]
                                 line = '\t'.join(line) + '\n'
                                 handle.write(line)
